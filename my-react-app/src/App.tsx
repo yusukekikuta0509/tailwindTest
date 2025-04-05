89d95e89d8a6fc71235a739cce7e5c0e19b64005
yusukekikuta0509 <yusukekikuta.05@gmail.com>
2025-04-06 00:57:38 +0900

    init

---
+++ my-react-app/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div className="p-4">
+        <h1 className="text-2xl font-bold">Hello Vite + React!</h1>
+      </div>
+    )
+  }
+  
+  export default App
+  
--- my-react-app/tailwind.config.js
+++ my-react-app/tailwind.config.js
@@ -1,9 +1,10 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
